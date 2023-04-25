c621c747c52f9b447e64b2149ea18e6fa5cae248
root <mgiraudo@ies21.edu.ar>
2023-04-25 12:21:15 -0300

    Archivo de la clase 8 - Conversion de Decimal a Romano

---
+++ Clase 8/frmRomanos.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Clase_8
+{
+    partial class frmRomanos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.txtSalida = new System.Windows.Forms.TextBox();
+            this.btnEntrada = new System.Windows.Forms.Button();
+            this.btnSalida = new System.Windows.Forms.Button();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.lblEntrada = new System.Windows.Forms.Label();
+            this.lblSalida = new System.Windows.Forms.Label();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Enabled = false;
+            this.txtEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtEntrada.Location = new System.Drawing.Point(27, 53);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(370, 23);
+            this.txtEntrada.TabIndex = 0;
+            // 
+            // txtSalida
+            // 
+            this.txtSalida.Enabled = false;
+            this.txtSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtSalida.Location = new System.Drawing.Point(27, 136);
+            this.txtSalida.Name = "txtSalida";
+            this.txtSalida.Size = new System.Drawing.Size(370, 23);
+            this.txtSalida.TabIndex = 1;
+            // 
+            // btnEntrada
+            // 
+            this.btnEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnEntrada.Location = new System.Drawing.Point(431, 47);
+            this.btnEntrada.Name = "btnEntrada";
+            this.btnEntrada.Size = new System.Drawing.Size(85, 31);
+            this.btnEntrada.TabIndex = 2;
+            this.btnEntrada.Text = "...";
+            this.btnEntrada.UseVisualStyleBackColor = true;
+            this.btnEntrada.Click += new System.EventHandler(this.btnEntrada_Click);
+            // 
+            // btnSalida
+            // 
+            this.btnSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnSalida.Location = new System.Drawing.Point(431, 134);
+            this.btnSalida.Name = "btnSalida";
+            this.btnSalida.Size = new System.Drawing.Size(85, 31);
+            this.btnSalida.TabIndex = 3;
+            this.btnSalida.Text = "...";
+            this.btnSalida.UseVisualStyleBackColor = true;
+            this.btnSalida.Click += new System.EventHandler(this.btnSalida_Click);
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnGenerar.Location = new System.Drawing.Point(113, 177);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(85, 31);
+            this.btnGenerar.TabIndex = 4;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // lblEntrada
+            // 
+            this.lblEntrada.AutoSize = true;
+            this.lblEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblEntrada.Location = new System.Drawing.Point(24, 20);
+            this.lblEntrada.Name = "lblEntrada";
+            this.lblEntrada.Size = new System.Drawing.Size(66, 20);
+            this.lblEntrada.TabIndex = 5;
+            this.lblEntrada.Text = "Entrada";
+            // 
+            // lblSalida
+            // 
+            this.lblSalida.AutoSize = true;
+            this.lblSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblSalida.Location = new System.Drawing.Point(24, 102);
+            this.lblSalida.Name = "lblSalida";
+            this.lblSalida.Size = new System.Drawing.Size(53, 20);
+            this.lblSalida.TabIndex = 6;
+            this.lblSalida.Text = "Salida";
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            // 
+            // frmRomanos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 231);
+            this.Controls.Add(this.lblSalida);
+            this.Controls.Add(this.lblEntrada);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.btnSalida);
+            this.Controls.Add(this.btnEntrada);
+            this.Controls.Add(this.txtSalida);
+            this.Controls.Add(this.txtEntrada);
+            this.Name = "frmRomanos";
+            this.Text = "Romanos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEntrada;
+        private System.Windows.Forms.TextBox txtSalida;
+        private System.Windows.Forms.Button btnEntrada;
+        private System.Windows.Forms.Button btnSalida;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label lblEntrada;
+        private System.Windows.Forms.Label lblSalida;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.SaveFileDialog sfd;
+    }
+}
+
---
+++ Clase 8/frmRomanos.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Clase_8
+{
+    public partial class frmRomanos : Form
+    {
+        public frmRomanos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntrada_Click(object sender, EventArgs e)
+        {
+            ofd.ShowDialog();
+            txtEntrada.Text = ofd.FileName;
+
+        }
+
+        private void btnSalida_Click(object sender, EventArgs e)
+        {
+            sfd.ShowDialog();
+            txtSalida.Text = sfd.FileName;
+        }
+
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            string entrada = txtEntrada.Text;
+            string salida = txtSalida.Text;
+
+
+            StreamReader sr = new StreamReader(entrada);
+            StreamWriter sw = new StreamWriter(salida);
+
+
+            sw.WriteLine("Decimal\t\tRomano");
+
+            while (!(sr.EndOfStream))
+            {
+                string numero = sr.ReadLine();
+                string linea = numero + "\t\t";
+
+                if (int.Parse(numero) <= 3999)
+                {
+                    linea = linea + Romano(int.Parse(numero));
+                }
+                else
+                {
+                    linea = linea + "No se puede converir";
+                }
+
+                sw.WriteLine(linea);
+
+            }
+            sr.Close();
+            sr.Dispose();
+
+            sw.Close();
+            sw.Dispose();
+
+            MessageBox.Show("Archivo generado");
+
+        }
+
+        private string Romano(int num)
+        {
+            string numRomano = "";
+
+            //MIL
+            int miles = num / 1000;
+            int resto = num % 1000;
+
+            //CIEN
+            int centenas = resto / 100;
+            resto = resto % 100;
+            
+            //DIEZ
+            int decenas = resto / 10;
+            resto = resto % 10;
+
+            //UNO
+            int unidad = resto / 1;
+
+            switch(miles)
+            {
+                case 1: numRomano = numRomano + "M";
+                    break;
+                case 2:
+                    numRomano = numRomano + "MM";
+                    break;
+                case 3:
+                    numRomano = numRomano + "MMM";
+                    break;
+            }
+
+            switch (centenas)
+            {
+                case 1:
+                    numRomano = numRomano + "C";
+                    break;
+                case 2:
+                    numRomano = numRomano + "CC";
+                    break;
+                case 3:
+                    numRomano = numRomano + "CCC";
+                    break;
+                case 4:
+                    numRomano = numRomano + "CD";
+                    break;
+                case 5:
+                    numRomano = numRomano + "D";
+                    break;
+                case 6:
+                    numRomano = numRomano + "DC";
+                    break;
+                case 7:
+                    numRomano = numRomano + "DCC";
+                    break;
+                case 8:
+                    numRomano = numRomano + "DCCC";
+                    break;
+                case 9:
+                    numRomano = numRomano + "CM";
+                    break;
+            }
+
+            switch (decenas)
+            {
+                case 1:
+                    numRomano = numRomano + "X";
+                    break;
+                case 2:
+                    numRomano = numRomano + "XX";
+                    break;
+                case 3:
+                    numRomano = numRomano + "XXX";
+                    break;
+                case 4:
+                    numRomano = numRomano + "XL";
+                    break;
+                case 5:
+                    numRomano = numRomano + "L";
+                    break;
+                case 6:
+                    numRomano = numRomano + "LX";
+                    break;
+                case 7:
+                    numRomano = numRomano + "LXX";
+                    break;
+                case 8:
+                    numRomano = numRomano + "LXXX";
+                    break;
+                case 9:
+                    numRomano = numRomano + "XC";
+                    break;
+            }
+
+            switch (unidad)
+            {
+                case 1:
+                    numRomano = numRomano + "I";
+                    break;
+                case 2:
+                    numRomano = numRomano + "II";
+                    break;
+                case 3:
+                    numRomano = numRomano + "III";
+                    break;
+                case 4:
+                    numRomano = numRomano + "IV";
+                    break;
+                case 5:
+                    numRomano = numRomano + "V";
+                    break;
+                case 6:
+                    numRomano = numRomano + "VI";
+                    break;
+                case 7:
+                    numRomano = numRomano + "VII";
+                    break;
+                case 8:
+                    numRomano = numRomano + "VIII";
+                    break;
+                case 9:
+                    numRomano = numRomano + "IX";
+                    break;
+            }
+
+            return numRomano;
+        }
+    }
+}
