25fc0561692717275eaf0bc5d5ffb8d48442bca0
root <mgiraudo@ies21.edu.ar>
2023-06-05 12:06:59 -0300

    Archivos de la clase 4 - dibujando sobre picturesBox

---
+++ parcial 2/Clase 4/frmPictureBox.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Clase_4
+{
+    partial class frmPictureBox
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.btnGraficar1 = new System.Windows.Forms.Button();
+            this.btnGraficar2 = new System.Windows.Forms.Button();
+            this.btnGraficar3 = new System.Windows.Forms.Button();
+            this.btnGraficar4 = new System.Windows.Forms.Button();
+            this.btnTablero = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic
+            // 
+            this.pic.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic.Location = new System.Drawing.Point(36, 22);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(400, 400);
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            // 
+            // btnGraficar1
+            // 
+            this.btnGraficar1.Location = new System.Drawing.Point(483, 22);
+            this.btnGraficar1.Name = "btnGraficar1";
+            this.btnGraficar1.Size = new System.Drawing.Size(254, 37);
+            this.btnGraficar1.TabIndex = 1;
+            this.btnGraficar1.Text = "GRAFICAR LINEAS";
+            this.btnGraficar1.UseVisualStyleBackColor = true;
+            this.btnGraficar1.Click += new System.EventHandler(this.btnGraficar_Click);
+            // 
+            // btnGraficar2
+            // 
+            this.btnGraficar2.Location = new System.Drawing.Point(483, 77);
+            this.btnGraficar2.Name = "btnGraficar2";
+            this.btnGraficar2.Size = new System.Drawing.Size(254, 37);
+            this.btnGraficar2.TabIndex = 2;
+            this.btnGraficar2.Text = "GRAFICAR 4 ELIPSES ROJAS";
+            this.btnGraficar2.UseVisualStyleBackColor = true;
+            this.btnGraficar2.Click += new System.EventHandler(this.btnGraficar2_Click);
+            // 
+            // btnGraficar3
+            // 
+            this.btnGraficar3.Location = new System.Drawing.Point(483, 131);
+            this.btnGraficar3.Name = "btnGraficar3";
+            this.btnGraficar3.Size = new System.Drawing.Size(254, 37);
+            this.btnGraficar3.TabIndex = 3;
+            this.btnGraficar3.Text = "GRAFICAR ESCUDO RIVER";
+            this.btnGraficar3.UseVisualStyleBackColor = true;
+            this.btnGraficar3.Click += new System.EventHandler(this.btnGraficar3_Click);
+            // 
+            // btnGraficar4
+            // 
+            this.btnGraficar4.Location = new System.Drawing.Point(483, 184);
+            this.btnGraficar4.Name = "btnGraficar4";
+            this.btnGraficar4.Size = new System.Drawing.Size(254, 37);
+            this.btnGraficar4.TabIndex = 4;
+            this.btnGraficar4.Text = "GRAFICAR CIRCULO VERDE";
+            this.btnGraficar4.UseVisualStyleBackColor = true;
+            this.btnGraficar4.Click += new System.EventHandler(this.btnGraficar4_Click);
+            // 
+            // btnTablero
+            // 
+            this.btnTablero.Location = new System.Drawing.Point(483, 237);
+            this.btnTablero.Name = "btnTablero";
+            this.btnTablero.Size = new System.Drawing.Size(254, 37);
+            this.btnTablero.TabIndex = 5;
+            this.btnTablero.Text = "TAREA: TABLERO DAMAS";
+            this.btnTablero.UseVisualStyleBackColor = true;
+            this.btnTablero.Click += new System.EventHandler(this.btnTablero_Click);
+            // 
+            // frmPictureBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 450);
+            this.Controls.Add(this.btnTablero);
+            this.Controls.Add(this.btnGraficar4);
+            this.Controls.Add(this.btnGraficar3);
+            this.Controls.Add(this.btnGraficar2);
+            this.Controls.Add(this.btnGraficar1);
+            this.Controls.Add(this.pic);
+            this.Name = "frmPictureBox";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmPictureBox_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button btnGraficar1;
+        private System.Windows.Forms.Button btnGraficar2;
+        private System.Windows.Forms.Button btnGraficar3;
+        private System.Windows.Forms.Button btnGraficar4;
+        private System.Windows.Forms.Button btnTablero;
+    }
+}
+
---
+++ parcial 2/Clase 4/frmPictureBox.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase_4
+{
+    public partial class frmPictureBox : Form
+    {
+        public frmPictureBox()
+        {
+            InitializeComponent();
+        }
+
+        Graphics gra;
+
+        private void frmPictureBox_Load(object sender, EventArgs e)
+        {
+            gra = pic.CreateGraphics();
+        }
+        private void btnGraficar_Click(object sender, EventArgs e)
+        {
+            gra.DrawLine(Pens.Black, 0, 0, 399, 399);
+            gra.DrawLine(Pens.Black, 399, 0, 0, 399);
+            gra.DrawLine(Pens.Black, 0, 199, 399, 199);
+            gra.DrawLine(Pens.Black, 199, 0, 199, 399);
+
+
+
+        }
+
+        private void btnGraficar2_Click(object sender, EventArgs e)
+        {
+            gra.DrawEllipse(Pens.Red, 0, 0, 200, 200);
+            gra.DrawEllipse(Pens.Red, 200, 0, 200, 200);
+            gra.DrawEllipse(Pens.Red, 0, 200, 200, 200);
+            gra.DrawEllipse(Pens.Red, 200, 200, 200, 200);
+
+        }
+
+        private void btnGraficar3_Click(object sender, EventArgs e)
+        {
+            Pen myLapiz = new Pen(Color.Red, 100);
+            gra.DrawLine(myLapiz, 399, 0, 0, 399);
+        }
+
+        private void btnGraficar4_Click(object sender, EventArgs e)
+        {
+            Pen myLapiz = new Pen(Color.Green, 10);
+            gra.DrawEllipse(myLapiz, 0, 0, 399, 399);
+        }
+
+        private void btnTablero_Click(object sender, EventArgs e)
+        {
+            // Hacer un tablero compuesto por muchos cuadrados y cada cuadrado de colores intercalados
+        }
+    }
+}
